58219920662e55b7a1ed4eaccf3d636419a4ab91
Batchu Sai Akhil <batchusai.akhil@mrcooper.com>
2020-12-22 00:10:32 +0530

    Added cors fix

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(err => console.error('not able to connect '));
 
 app.use(bodyParser.json());
+
+//handling cors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
 app.use(isAuth);
 
 
